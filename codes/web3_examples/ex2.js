c58257ba69a76167705cf7b36ed253fb38ed3f30
rszheng <zhengrs@sustc.edu.cn>
2018-07-08 21:39:28 +0800

    Create ex2.js

---
+++ codes/web3_examples/ex2.js
@@ -0,0 +1,16 @@
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var blockID = 1234567;
+var info = web3.eth.getBlock(blockID);
+// console.log(info);
+
+var transaction = web3.eth.getTransactionFromBlock(blockID,0);
+
+// Alternatively, use the following (only when you are not running a light
+// node):
+//
+/* var txHash = info.transactions[0];
+   var transaction = web3.eth.getTransaction(txHash);
+*/
